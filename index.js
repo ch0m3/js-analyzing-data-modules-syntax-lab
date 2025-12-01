61fb1308a2e4a4bf6b28cf6f398ac3f3b64f1797
Chome <chomeallan@gmail.com>
2025-12-01 20:35:18 +0300

    merging

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+//create a function that merges all usernames into one
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users:[]
+  };
 
+  for (const userArray of args){
+    combinedObject.users.push(...userArray)
+  }
+  const today = new Date();
+  combinedObject.merge_date = today.toString('M/d/yyyy');
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+  return combinedObject;
+}
+
+module.exports = { combineUsers };
+
+const result =combineUsers(
+  ['alice', 'kennedy'],
+  ['mzesh'],
+  ['babe', 'rico']
+);
+
+console.log(result);
+
